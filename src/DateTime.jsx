5838f8c42ecc1ebe6060c2afcf5753a829951e27
Weslley <weslley.paiva@vegalux.com.br>
2024-05-08 09:57:10 -0300

    fix: localeText to pt-br

--- src/DateTime.jsx
+++ src/DateTime.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { DateTimePicker } from '@mui/x-date-pickers';
-import { ptBR } from 'date-fns/locale'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { ptBR } from '@mui/x-date-pickers/locales';
+import { pt } from 'date-fns/locale';
+
+const brazilianLocale = ptBR.components.MuiLocalizationProvider.defaultProps.localeText;
 
 function DateTime() {
     return (
       <div>
         <h2>Datas e horários</h2>
-          <LocalizationProvider locale={ptBR} dateAdapter={AdapterDateFns}>
+          <LocalizationProvider adapterLocale={pt} dateAdapter={AdapterDateFns} localeText={brazilianLocale}>
             <DateTimePicker 
               sx={{ marginRight: 1 }} 
               label="Início"
               inputFormat="dd-MMMM-yyyyy"
             />
           </LocalizationProvider>
-          <LocalizationProvider locale={ptBR} dateAdapter={AdapterDateFns}>
+          <LocalizationProvider adapterLocale={pt} dateAdapter={AdapterDateFns} localeText={brazilianLocale}>
             <DateTimePicker 
               label="Término"
               inputFormat="dd-MMMM-yyyyy"
