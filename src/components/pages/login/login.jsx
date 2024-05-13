a8f478c4c68c0dc501f1d2a0810c293cc85597b0
Weslley <weslley.paiva@vegalux.com.br>
2024-05-13 16:00:07 -0300

    wip

--- src/components/pages/login/login.jsx
+++ src/components/pages/login/login.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Input } from "../../atoms/input/input";
 import { LoginContainer, LoginStyles, Title } from './login-styles';
 import { Button } from '../../atoms/button/button';
+import { UserService } from '../../../service/user-service/user-service';
 
 
 export const Login = () => {
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
 
+
+  const userService = new UserService()
+
+  useEffect(() => {
+
+    userService.log({
+      email: 'teste@teste.com',
+      password: 'teste'
+    }).then((res) => console.log(res))
+
+  }, [])
 
   return (
     <LoginContainer>
---
+++ src/service/user-service/user-service.js
@@ -0,0 +1,34 @@
+import AuthService from "../auth-service/auth-service.js"
+
+export class UserService {
+
+    path
+    login
+    auth
+
+    constructor() {
+        this.path = 'user'
+        this.login = 'login'
+        this.auth = new AuthService()
+    }
+
+    async log(request){
+        const response = await this.auth.getApi().post(this.login, request)
+
+        console.log(response)
+
+        // const autentication = await this.auth.setAuthorizationHeader(response.token)
+
+        return response
+    }
+
+    async findAll() {
+        const response = await this.auth.getApi().get(this.path)
+        return response.data
+    }
+
+    async create(request) {
+        const response = await this.auth.getApi().post(this.path, request)
+        return response.data
+    }
+}
