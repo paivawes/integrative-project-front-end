e6ee28b8a994f1a75b8292da1078165a6ff10d61
Leandro Fernandes <leandroofnd@gmail.com>
2024-05-13 23:06:58 -0300

    Making the page more dynamic

--- src/DateTime.jsx
+++ src/DateTime.jsx
@@ -1,28 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { DateTimePicker } from '@mui/x-date-pickers';
-import { ptBR } from 'date-fns/locale'
+import React from 'react';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { ptBR } from '@mui/x-date-pickers/locales';
+import { pt } from 'date-fns/locale';
 
-function DateTime() {
-    return (
-      <div>
-        <h2>Datas e horários</h2>
-          <LocalizationProvider locale={ptBR} dateAdapter={AdapterDateFns}>
-            <DateTimePicker 
-              sx={{ marginRight: 1 }} 
-              label="Início"
-              inputFormat="dd-MMMM-yyyyy"
-            />
-          </LocalizationProvider>
-          <LocalizationProvider locale={ptBR} dateAdapter={AdapterDateFns}>
-            <DateTimePicker 
-              label="Término"
-              inputFormat="dd-MMMM-yyyyy"
-            />
-          </LocalizationProvider>
-      </div>
-    );
-  }
-  
-  export default DateTime;
+const brazilianLocale = ptBR.components.MuiLocalizationProvider.defaultProps.localeText;
+
+function DateTime({ onChangeStartDate, onChangeEndDate }) {
+  return (
+    <div>
+      <h2>Datas e horários</h2>
+      <LocalizationProvider adapterLocale={pt} dateAdapter={AdapterDateFns} localeText={brazilianLocale}>
+        <DateTimePicker 
+          sx={{ marginRight: 1 }} 
+          label="Início"
+          inputFormat="dd-MMMM-yyyyy"
+          onChange={onChangeStartDate}
+        />
+      </LocalizationProvider>
+      <LocalizationProvider adapterLocale={pt} dateAdapter={AdapterDateFns} localeText={brazilianLocale}>
+        <DateTimePicker 
+          label="Término"
+          inputFormat="dd-MMMM-yyyyy"
+          onChange={onChangeEndDate}
+        />
+      </LocalizationProvider>
+    </div>
+  );
+}
+
+export default DateTime;
--- src/components/atoms/button/button.jsx
+++ src/components/atoms/button/button.jsx
@@ -1,12 +1,13 @@
 import { ButtonStyles } from "./button-styles"
 
-export const Button = ({ onClick, label }) => {
+export const Button = ({ onClick, label, disabled }) => {
     return (
         <ButtonStyles
             sx={{ margin: '20px'}}
             size='medium'
             variant={'contained'}
             onClick={onClick}
+            disabled={disabled}
         >
             {label}
         </ButtonStyles>
