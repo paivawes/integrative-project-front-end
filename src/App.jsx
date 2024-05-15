4ef26c22cefac97618c6f823a6d7c6ce5ca20322
Weslley <weslley.paiva@vegalux.com.br>
2024-05-15 15:36:37 -0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './components/pages/login/login';
 import { Schedule } from './components/pages/schedule/schedule';
 import './App.css'
-import Rotas from './rotas/Rotas'
 
 
 
