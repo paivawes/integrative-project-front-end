08f6f76dcf43f11ae905dded14198d8f2964f5e3
Weslley <weslley.paiva@vegalux.com.br>
2024-05-15 15:39:14 -0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './components/pages/login/login';
 import { Schedule } from './components/pages/schedule/schedule';
-import './App.css'
-
-
 
 export default function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/agendamento" element={<Schedule />} />
-        </Routes>
-      </Router>
-  );
+  return(
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/agendamento" element={<Schedule />} />
+    </Routes>
+  </Router>
+);
 }
-
