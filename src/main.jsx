beb90431bd4290fa256383b4fa8e0310f8283024
paivawes <weslleysilvapaiva@outlook.com>
2024-05-19 19:15:18 -0300

    use context

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { UserProvider } from './context/user-context'
 import './index.css'
 import { UserProvider } from './context/user-context.jsx'
 
