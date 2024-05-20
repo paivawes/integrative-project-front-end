2b53e5816e3e49ff4fe09f7b98050deb8223f8b6
CarlosVaz <131931128+carlos-vaz-dev@users.noreply.github.com>
2024-05-19 22:25:22 -0300

    Revert "Merge branch 'main' of https://github.com/paivawes/integrative-project-frontend"
    This reverts commit 8b8cc220b71a0c2349e65e8756ac83f162820223, reversing
    changes made to eb5169ac616d4151a8daf9d3f7d79873076fe9b3.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { UserProvider } from './context/user-context'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </React.StrictMode>,
 )
