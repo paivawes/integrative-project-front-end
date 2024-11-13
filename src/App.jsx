9337d03c94f1b03652f7f5cbfb772269ab8f3b12
paivawes <weslleysilvapaiva@outlook.com>
2024-11-13 19:30:22 -0300

    feat: acessibilidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './components/pages/login/login';
 import { Schedule } from './components/pages/schedule/schedule';
@@ -6,7 +6,32 @@ import { Cadastro } from './components/pages/cadastro/cadastro';
 import { Register } from './components/pages/create-user/create-user';
 
 export default function App() {
+  useEffect(() => {
+    const loadVLibras = () => {
+      if (!document.querySelector('script[src="https://vlibras.gov.br/app/vlibras-plugin.js"]')) {
+        const script = document.createElement('script');
+        script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+        script.async = true;
+        script.onload = () => {
+          new window.VLibras.Widget('https://vlibras.gov.br/app');
+        };
+        document.body.appendChild(script);
+      }
+    };
+  
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', loadVLibras);
+    } else {
+      loadVLibras();
+    }
+  
+    return () => {
+      document.removeEventListener('DOMContentLoaded', loadVLibras);
+    };
+  }, []);
+
   return (
+    <div className='App'>
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
@@ -16,5 +41,6 @@ export default function App() {
         <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </Router>
+    </div>
   );
 }
